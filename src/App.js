00787e4d2f84c3483a5262b7d7dee9e2c2f3d796
Nitin3993 <nitinporwal99@gmail.com>
2019-06-08 16:20:27 +0530

    Form Created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,59 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <React.Fragment>
+      <div className="wrapper">
+        <div className="form-wrapper"> 
+          <h1>Create Account</h1>
+          <form  noValidate>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+              <input type="text" 
+                noValidate
+                name="firstName"
+                placeholder="First Name"
+                className=""
+                
+              />
+            </div>
+
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" 
+                noValidate
+                name="lastName"
+                placeholder="Last Name"
+                className=""
+                
+              />
+            </div>
+
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input type="text" 
+                noValidate
+                name="email"
+                placeholder="Email"
+                className=""
+                
+              />
+            </div>
+
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input type="text" 
+                noValidate
+                name="password"
+                placeholder="Password"
+                className=""
+                
+              />
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
